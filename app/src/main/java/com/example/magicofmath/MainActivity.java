9df1b082c239be83378be92108cb928f660015af
tv <volianskyitv@gmail.com>
2019-10-27 20:52:23 +0200

    refactoring of business logic

--- app/src/main/java/com/example/magicofmath/MainActivity.java
+++ app/src/main/java/com/example/magicofmath/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.PorterDuff;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -31,24 +33,66 @@ public class MainActivity extends AppCompatActivity {
     int finalPointX;
     int finalPointY;
     // int mainSize = 200;
+    private SeekBar seekBar;
+
     ArrayList<Integer> arrayListPointsX = new ArrayList<>();
     ArrayList<Integer> arrayListPointsY = new ArrayList<>();
     ArrayList<Integer> arrayListPointsXsorted = new ArrayList<>();
     MyPoint myPoint = new MyPoint();
     ArrayList<MyPoint> myPointArrayList = new ArrayList<>();
+private     GraphView graph;
+    private  PointsGraphSeries<DataPoint> series;
+    private  PointsGraphSeries<DataPoint> series2;
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initSeekBarView();
         initBL();
         initView();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void initView() {
-        createGrapg2();
+        createGrapg2appendData();
+
+    }
+
+    private void initSeekBarView() {
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                arrayListPointsX.clear();
+                arrayListPointsY.clear();
+                myPointArrayList.clear();
+                pointBx= progress;
+                createPointsForFractal();
+                createGrapg2resetData();
+                graph.getSecondScale().addSeries(series2);
+
+               // initBL();
+              //  initView();
+              //  Toast.makeText(MainActivity.this, "" + progress, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -60,11 +104,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    private void createGrapg2() {
+    private void createGrapg2appendData() {
         int x = 0, y = 0;
+        graph = (GraphView) findViewById(R.id.graph);
         sort();
-        PointsGraphSeries<DataPoint> series;
-        GraphView graph = (GraphView) findViewById(R.id.graph);
         series = new PointsGraphSeries<DataPoint>();
         for (int i = 0; i < arrayListPointsX.size(); i++) {
             //  x = arrayListPointsX.get(i);      y = arrayListPointsY.get(i);
@@ -78,10 +121,36 @@ public class MainActivity extends AppCompatActivity {
         //
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(getMaxPoints(pointAx,pointBx,pointCx));
+        graph.getViewport().setMaxX(getMaxPoints(pointAx, pointBx, pointCx));
         graph.getViewport().setYAxisBoundsManual(true);
         graph.getViewport().setMinY(0);
-        graph.getViewport().setMaxY(getMaxPoints(pointAy,pointBy,pointCy));
+        graph.getViewport().setMaxY(getMaxPoints(pointAy, pointBy, pointCy));
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void createGrapg2resetData() {
+        int x = 0, y = 0;
+        sort();
+        series2 = new PointsGraphSeries<DataPoint>();
+        for (int i = 0; i < arrayListPointsX.size(); i++) {
+            //  x = arrayListPointsX.get(i);      y = arrayListPointsY.get(i);
+            //  x=  myPoint.getPointXcoordinate();  y=myPoint.getPointYcoordinate();
+            x = myPointArrayList.get(i).getPointXcoordinate() - 1;
+            y = myPointArrayList.get(i).getPointYcoordinate() - 1;
+           // series2.resetData(data());
+            series2.appendData(new DataPoint(x, y), true, 999999999);
+
+        }
+      //  graph.addSeries(series2);
+        graph.getLegendRenderer().setWidth(40000);
+        //
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(getMaxPoints(pointAx, pointBx, pointCx));
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(getMaxPoints(pointAy, pointBy, pointCy));
 
     }
 
@@ -117,15 +186,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public DataPoint[] data() {
+        int x = 0, y = 0;
+
         DataPoint[] values = new DataPoint[arrayListPointsX.size()];
-        for (int i = 0; i < arrayListPointsX.size(); i++) {
-            DataPoint v = new DataPoint(arrayListPointsX.get(i), arrayListPointsY.get(i));
-            values[i] = v;
+
+            for (int i = 0; i < arrayListPointsX.size(); i++) {
+                //  x = arrayListPointsX.get(i);      y = arrayListPointsY.get(i);
+                //  x=  myPoint.getPointXcoordinate();  y=myPoint.getPointYcoordinate();
+                x = myPointArrayList.get(i).getPointXcoordinate() - 1;
+                y = myPointArrayList.get(i).getPointYcoordinate() - 1;
+                DataPoint v = new DataPoint(x, y);
+                values[i] = v;
         }
         return values;
     }
 
     private void createPointsForFractal() {
+
+
         arrayListPointsX.add(pointStartX);
         arrayListPointsY.add(pointStartY);
         myPointArrayList.add(new MyPoint(0, pointStartX, pointStartY));
